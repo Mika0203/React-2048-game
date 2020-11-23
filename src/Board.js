f4fa1623935ed8a239305ebe21940b1878fe59b4
Mik_a <ilemik@youcanstar.com>
2020-11-23 17:15:52 +0900

    square 추가 및 css 파일 추가

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import './Board.css';
+
+class Square extends React.Component {
+    render(){
+        return <div className='square'></div>
+    }
+}
 
 class Board extends React.Component {
-    render(){
-      return <div></div>
+    render() {
+        return <div className='board'>
+            <div>
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div>
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div>
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </div>
     }
-  }
-  
+}
+
 
 export default Board
