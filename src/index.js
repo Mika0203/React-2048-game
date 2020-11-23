d567bc792d5586dfb7369c511184a0e52e30da5e
Mik_a <ilemik@youcanstar.com>
2020-11-23 17:02:07 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Board extends React.Component {
+  render(){
+    return <div></div>
+  }
+}
+
+// import App from './App';
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
+
